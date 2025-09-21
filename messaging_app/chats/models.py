c5207c5e25ce88510e6d9524676d0dfa894731c8
Obaro Awowede <akpobaroawowede@gmail.com>
2025-09-21 12:49:10 +0100

    Created User model, which extends the AbstractUser model

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
 
-# Create your models here.
+class User(AbstractUser):
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    first_name = models.CharField(max_length=215, null=False)
+    last_name = models.CharField(max_length=215, null=False)
+    email = models.EmailField(unique=True, null=False)
+    password_hash = models.CharField(max_length=215, null=False)
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
+    role = models.CharField(
+        max_length=20,
+        choices=[
+            ('guest','Guest'),
+            ('host','Host'),
+            ('admin','Admin')
+        ],
+        null= False
+    )
+    created_at = models.DateField(auto_now_add=True)
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
