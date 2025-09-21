2b642f66e617173b58c9f0cbb6a2ec876eb92aa9
Obaro Awowede <akpobaroawowede@gmail.com>
2025-09-21 15:51:40 +0100

    Updated serializers: added validation of email in UserSerializer and validation of number of participants in ConversationSerializer

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -7,9 +7,14 @@ class UserSerializer(serializers.ModelSerializer):
         fields =['user_id','first_name','last_name','email',
                  'phone_number','role','created_at']
         read_only_fields = ['user_id','created_at']
+    
+    def validate_email(self,value):
+        if User.objects.filter(email = value).exists():
+            raise serializers.ValidationError("A user with this email exists")
         
 class MessageSerializer(serializers.ModelSerializer):
     sender = UserSerializer(read_only = True)
+    message_body = serializers.CharField(required=True)
     class Meta:
         model = Message
         fields = ['message_id','sender_id','conversation',
@@ -18,9 +23,18 @@ class MessageSerializer(serializers.ModelSerializer):
         
 class ConversationSerializer(serializers.ModelSerializer):
     participants = UserSerializer(read_only = True)
-    messages = MessageSerializer(read_only = True)
+    messages = serializers.SerializerMethodField()
     
     class Meta:
         model = Conversation
         fields = ['conversation_id','participants','messages', 'created_at']
         read_only_fields = ['conversation_id', 'created_at']
+        
+    def get_messages(self, obj):
+        messages = obj.messages.order_by('-sent_at')
+        return MessageSerializer(messages, many=True, context=self.context).data
+
+    def validate_participants(self, value):
+        if len(value) < 2:
+            raise serializers.ValidationError("A conversation must have at least two participants.")
+        return value
