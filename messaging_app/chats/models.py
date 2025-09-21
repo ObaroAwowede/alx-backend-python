8f85cdfc20e73ea138470758869adbdc7a857cfd
Obaro Awowede <akpobaroawowede@gmail.com>
2025-09-21 13:43:29 +0100

    Created the Message and conversation models

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -19,5 +19,36 @@ class User(AbstractUser):
         null= False
     )
     created_at = models.DateField(auto_now_add=True)
+    class Meta:
+        indexes = [
+            models.Index(fields=['email'])
+        ]
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+
+class Conversation(models.Model):
+    conversation_id = models.UUIDField(primary_key=True,default=uuid.uuid4)
+    participants_id = models.ManyToManyField(User, related_name='conversations')
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        indexes = [
+            models.Index(fields=['created_at'])
+        ]
+        
+    def __str__(self):
+        return f"Conversation {self.conversation_id}"
+
+class Message(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
+    message_body = models.TextField(null=False)
+    sent_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['sender']),
+            models.Index(fields=['conversation']),
+            models.Index(fields=['sent_at'])
+        ]
