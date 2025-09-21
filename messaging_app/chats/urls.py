f6193b9723dbaa118b231ebca92cc2dc8aa63528
Obaro Awowede <akpobaroawowede@gmail.com>
2025-09-21 19:49:45 +0100

    Implemented the NestedDefaultRouter in chats/urls.py, also updated project urls.py to include the DRF authentication endpoint

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -3,9 +3,11 @@ from rest_framework import routers
 from .views import ConversationViewSet, MessageViewSet
 
 router = routers.DefaultRouter()
-router.register(r'conversations', ConversationViewSet, basename='conversation')
-router.register(r'messages', MessageViewSet, basename='message')
+router.register(r'conversations', ConversationViewSet)
+conversations_router = routers.NestedDefaultRouter(router, r'conversations', lookup='conversation')
+conversations_router.register(r'messages', MessageViewSet, basename='conversation-messages')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include(conversations_router.urls)),
 ]
